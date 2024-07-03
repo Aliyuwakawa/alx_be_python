abd007e3dea3eac3f5524ed35311fcf9f15f804e
Aliyuwakawa <aliyuwakawa76@gmail.com>
2024-07-03 10:26:02 -0700

    s

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
-def perform_operation(num1(float), num2(float), operation('add', 'subtact', 'multiply', 'divide')):
+def perform_operation(num1, num2, operation):
     """
     performs basic arithmetic operations based on the input  parameters.
     """
 
     if operation == 'add':
-        result = "float(num1 + num2)"
+        result = float(num1 + num2)
     elif operation == 'subtract':
-        result = "float(num1 - num2)"
+        result = float(num1 - num2)
     elif operation == 'multiplication':
-        result = "float(num1 * num2)"
+        result = float(num1 * num2)
     elif operation == 'divide':
         if num2 = 0:
             result = "Erro, division by zero os undefined"
