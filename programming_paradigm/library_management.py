f5a82ee85a219af83cc616cb8cd2d5662d785997
Aliyuwakawa <aliyuwakawa76@gmail.com>
2024-07-13 06:29:44 -0700

    L

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -19,3 +19,33 @@ class Book:
     def is_available(self):
         return not self._is_checked_out
 
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return f"{title} has been checked out."
+        return f"{title} is not available or does not exist."
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return f"{title} has been returned."
+        return f"{title} is not checked out or does not exist."
+
+    def list_available_books(self):
+        available_books = [f"{book.title} by {book.author}" for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(book)
+        else:
+            print("No available books.")
+
