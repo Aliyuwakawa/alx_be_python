d826faca787f159039f591024339d878fdf6d248
Aliyuwakawa <aliyuwakawa76@gmail.com>
2024-07-17 07:45:20 -0700

    l

---
+++ oop/library_system.py
@@ -0,0 +1,49 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}kB"
+
+class PrintBook:
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.book = []
+
+    def add_book(self, book):
+        self.book.append(book)
+
+    def list_books(self):
+        for book in self.book:
+            print(book)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
